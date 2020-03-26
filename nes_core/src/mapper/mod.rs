6d5dd9d53f181c10a285190830a4d56e876acd06
bengdahl <bengdahl341@gmail.com>
2020-03-25 22:31:56 -0400

    Add the UxROM mapper and document the Mapper trait

--- nes_core/src/mapper/mod.rs
+++ nes_core/src/mapper/mod.rs
@@ -1,17 +1,27 @@
 mod nrom;
 pub mod dummy;
 mod mmc1;
+mod uxrom;
 
 use crate::error::*;
 use crate::cart::{Ines, Mirroring};
 
 /// Represents a memory banking method for a cartridge.
 pub trait Mapper {
+    /// Returns the name of the mapper
     fn name(&self) -> &'static str;
+    /// Performs a read from the cartridge
     fn read(&self, ines: &Ines, addr: u16) -> u8;
+    /// Performs a write to the cartridge
     fn write(&mut self, ines: &Ines, addr: u16, v: u8);
+    /// Resets the mapper
     fn reset(&mut self);
+    /// Clones the mapper's state
     fn clone(&self) -> Box<dyn Mapper + Send + Sync>;
+    /// Gets the nametable mirroring mode.
+    /// 
+    /// The default implementation always selects the mirroring mode defined 
+    /// in the iNES header.
     fn mirroring(&self) -> Option<Mirroring> {
         None
     }
@@ -21,6 +31,7 @@ pub fn from_ines_id(id: u16) -> Result<Box<dyn Mapper + Send + Sync>> {
     match id {
         0 => Ok(Box::new(nrom::NROM::new())),
         1 => Ok(Box::new(mmc1::MMC1::new())),
+        2 => Ok(Box::new(uxrom::UxROM::new())),
         _ => Err(Error::format_err(format!("Invalid mapper ID: {}", id)))
     }
 }
---
+++ nes_core/src/mapper/uxrom.rs
@@ -0,0 +1,78 @@
+use super::Mapper;
+use crate::cart::Ines;
+
+const BANK_SIZE: usize = 0x4000;
+
+#[allow(non_snake_case)]
+#[derive(Copy, Clone)]
+pub struct UxROM {
+    bank_select: u8,
+    chr_ram: [u8; 0x2000]
+}
+
+impl UxROM {
+    pub fn new() -> Self {
+        UxROM {
+            bank_select: 0,
+            chr_ram: [0; 0x2000]
+        }
+    }
+
+    fn get_last_bank<'a>(&self, ines: &'a Ines) -> &'a [u8] {
+        let prg_rom = ines.prg_rom_slice();
+        &prg_rom[prg_rom.len() - BANK_SIZE ..]
+    }
+
+    fn get_first_bank<'a>(&self, ines: &'a Ines) -> &'a [u8] {
+        let prg_rom = ines.prg_rom_slice();
+        let offset = self.bank_select as usize * BANK_SIZE;
+        &prg_rom[offset .. offset + BANK_SIZE]
+    }
+}
+
+impl Mapper for UxROM {
+    fn name(&self) -> &'static str {
+        "UxROM"
+    }
+    fn read(&self, ines: &Ines, addr: u16) -> u8 {
+        match addr {
+            0x0000..=0x1FFF => {
+                let chr = ines.chr_rom_slice();
+                if let Some(chr) = chr {
+                    chr[addr as usize]
+                } else {
+                    self.chr_ram[addr as usize]
+                }
+            },
+
+            0x8000..=0xBFFF => {
+                let bank = self.get_first_bank(ines);
+                bank[addr as usize - 0x8000]
+            },
+            0xC000..=0xFFFF => {
+                let bank = self.get_last_bank(ines);
+                bank[addr as usize - 0xC000]
+            },
+
+            _ => 255
+        }
+    }
+    fn write(&mut self, ines: &Ines, addr: u16, v: u8) {
+        match addr {
+            0x0000..=0x1FFF if ines.chr_rom_slice().is_none() => {
+                self.chr_ram[addr as usize] = v;
+            },
+            0x8000..=0xFFFF => {
+                // The real boards usually used only the low 3-4 bits, im just going to use the full 8 bits
+                self.bank_select = v;
+            },
+            _ => {}
+        }
+    }
+    fn reset(&mut self) {
+        self.bank_select = 0;
+    }
+    fn clone(&self) -> Box<dyn Mapper + Send + Sync> {
+        Box::new(std::clone::Clone::clone(self))
+    }
+}
